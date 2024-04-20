4572275006ef36a01ea86a9f5a719f0cedcb6840
Caden Barton <cadenbarton@Cadens-MacBook-Air.local>
2024-04-20 17:09:49 -0600

    Added validation to description field and displaying error messages

--- src/main/java/com/learn/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/learn/springboot/myfirstwebapp/todo/Todo.java
@@ -1,11 +1,13 @@
 package com.learn.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
+import jakarta.validation.constraints.Size;
 
 public class Todo {
 	
 	private int id;
 	private String username;
+	@Size(min=10, message="Enter at least 10 characters")
 	private String description;
 	private LocalDate targetDate;
 	private boolean done;

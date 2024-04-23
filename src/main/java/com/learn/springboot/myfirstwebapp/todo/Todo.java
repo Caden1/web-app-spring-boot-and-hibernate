aa6a5d76485468b5921661cfa87c86ae1811f025
Caden Barton <cadenbarton@Cadens-MacBook-Air.local>
2024-04-23 17:21:25 -0600

    Added Entity to Todo bean to create Todo table in h2

--- src/main/java/com/learn/springboot/myfirstwebapp/todo/Todo.java
+++ src/main/java/com/learn/springboot/myfirstwebapp/todo/Todo.java
@@ -1,10 +1,17 @@
 package com.learn.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Todo {
 	
+	@Id
+	@GeneratedValue
 	private int id;
 	private String username;
 	@Size(min=10, message="Enter at least 10 characters")
